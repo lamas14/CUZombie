8e415e4ff2f99d5853b9e034a994ee92733fc33d
lamas14 <lamas14@mycu.concord.edu>
2013-06-24 19:27:06 -0400

    First Commit
    Basic setup. Black screen.

---
+++ src/com/saugatlama/cuzombie/Game.java
@@ -0,0 +1,81 @@
+package com.saugatlama.cuzombie;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable{
+	private static final long serialVersionUID = 1L;
+	
+	public static int WIDTH = 300;
+	public static int HEIGHT = WIDTH/16 * 9;
+	public static int SCALE = 3;
+	
+	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
+	
+	public Game(){
+		Dimension size = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
+	
+	public synchronized void start(){
+		running = true;
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public synchronized void stop(){
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run(){
+		while(running){
+			update();
+			render();
+		}
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void render(){
+		BufferStrategy bs = getBufferStrategy();
+		if(bs == null){
+			createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.dispose();
+		bs.show();
+	}
+	
+	public static void main(String[] args){
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("CU Zombie");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		game.start();
+	}
+}
