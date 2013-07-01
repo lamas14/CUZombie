1f7f4d1ec65502784ddb143ce702c08c70db5f33
lamas14 <lamas14@mycu.concord.edu>
2013-07-01 16:22:12 -0400

    projectile collision
    projectile removed when it collides with objects

--- src/com/saugatlama/cuzombie/entity/projectile/TrainerProjectile.java
+++ src/com/saugatlama/cuzombie/entity/projectile/TrainerProjectile.java
@@ -3,10 +3,10 @@ package com.saugatlama.cuzombie.entity.projectile;
 import com.saugatlama.cuzombie.graphics.Screen;
 import com.saugatlama.cuzombie.graphics.Sprite;
 
-public class TrainerProjectile extends Projectile{
+public class TrainerProjectile extends Projectile {
 
-	public static final int FIRERATE = 12; //Higher is slower
-	
+	public static final int FIRERATE = 23; // Higher is slower
+
 	public TrainerProjectile(int x, int y, double dir) {
 		super(x, y, dir);
 		range = 100;
@@ -16,24 +16,27 @@ public class TrainerProjectile extends Projectile{
 		nx = speed * Math.cos(angle);
 		ny = speed * Math.sin(angle);
 	}
-	
-	public void update(){
+
+	public void update() {
+		if ((level.tileCollision(x, y, nx, ny, 9))) remove();
 		move();
 	}
-	
-	protected void move(){
-		x += nx;
-		y += ny;
-		if(distance()>range) remove();
+
+	protected void move() {
+			x += nx;
+			y += ny;
+			if (distance() > range)
+				remove();
 	}
-	
+
 	private double distance() {
 		double dist = 0;
-		dist = Math.sqrt(Math.abs((xOrigin - x) * (xOrigin - x) + (yOrigin - y) * (yOrigin - y)));
+		dist = Math.sqrt(Math.abs((xOrigin - x) * (xOrigin - x) + (yOrigin - y)
+				* (yOrigin - y)));
 		return dist;
 	}
 
-	public void render(Screen screen){
-		screen.renderProjectile((int)x-5, (int)y-2, this);
+	public void render(Screen screen) {
+		screen.renderProjectile((int) x - 5, (int) y - 2, this);
 	}
 }
--- src/com/saugatlama/cuzombie/level/Level.java
+++ src/com/saugatlama/cuzombie/level/Level.java
@@ -49,6 +49,16 @@ public class Level {
 	private void time(){
 	}
 	
+	public boolean tileCollision(double x, double y, double xa, double ya, int size){
+		boolean solid = false;
+		for(int c = 0; c < 4; c++){
+			int xt = (((int)x + (int)xa) + c % 2 * size - 2 ) / 16;
+			int yt = (((int)y + (int)ya) + c / 2 * size +1) / 16;
+			if(getTile((int)xt, (int)yt).solid()) solid = true;	
+		}
+		return solid;
+	}
+	
 	public void render(int xScroll, int yScroll, Screen screen){
 		screen.setOffset(xScroll, yScroll);
 		int x0 = xScroll >> 4; // x0 = xScroll/16
@@ -75,6 +85,7 @@ public class Level {
 	}
 	
 	public void addProjectile(Projectile p){
+		p.init(this);
 		projectiles.add(p);
 	}
 	//Grass = green(0xFF00FF00)
